f44496f114ff7f6b4cefb6a941c1b17809c5420c
fushigo <rizky23ska@gmail.com>
2024-01-18 13:06:12 +0700

    thrid commit

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,7 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_app/app/data/constant/endpoint.dart';
+import 'package:peminjam_perpustakaan_app/app/data/provider/api_provider.dart';
+import 'package:dio/dio.dart' as dio;
 
 class RegisterController extends GetxController {
-  //TODO: Implement RegisterController
+  //TODO: Implement AddBookController
+  final GlobalKey<FormState> formkey= GlobalKey<FormState>();
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController telpController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
 
   final count = 0.obs;
   @override
@@ -20,4 +32,38 @@ class RegisterController extends GetxController {
   }
 
   void increment() => count.value++;
+
+  final loadingRegister = false.obs;
+  addRegister() async{
+    loadingRegister(true);
+    try{
+      FocusScope.of(Get.context!).unfocus();
+      formkey.currentState?.save();
+      if(formkey.currentState!.validate()) {
+        final response = await ApiProvider.instance().post(Endpoint.register,
+            data: {
+              "nama": namaController.text.toString(),
+              "username": usernameController.text.toString(),
+              "telp": telpController.text.toString(),
+              "alamat": alamatController.text.toString(),
+              "password": passwordController.text.toString(),
+
+            }
+        );
+        if(response.statusCode == 201){
+          Get.back();
+        }else{
+          Get.snackbar("Sorry","Register Gagal", backgroundColor: Colors.orange);
+        }
+      }
+      loadingRegister(false);
+    }on dio.DioException catch (e) {
+      loadingRegister(false);
+      Get.snackbar("Sorry", e.message ?? "",backgroundColor: Colors.red);
+    }catch(e) {
+      loadingRegister(false);
+      Get.snackbar("Error",e.toString(),backgroundColor: Colors.red);
+      throw Exception("Error: $e");
+    }
+  }
 }
